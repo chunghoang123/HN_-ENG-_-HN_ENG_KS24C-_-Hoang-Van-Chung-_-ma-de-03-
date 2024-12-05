bb77961ee3a755993c5261b2da9bfe320a747dd5
chunghoang123 <chungss7890@gmail.com>
2024-12-05 15:13:38 +0700

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -165,9 +165,9 @@ int main()
 			case 10:
 			{
 				printf("dao nguoc thu tu");
-				for(int i=0;i<n;i++)
+				for(int i=n-1;i>=0;i--)
 				{
-					printf("%d,"a[i-1]);
+					printf("a[%d]= %d",i,a[i]);
 				}
 				break;
 			}
